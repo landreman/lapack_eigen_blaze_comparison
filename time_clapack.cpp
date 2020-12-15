a87b0d9a9420533d527cadaedc4b4ff5c9614ef3
Matt Landreman <mattland@umd.edu>
2020-12-15 16:56:37 -0500

    Changed from vector to valarray

--- time_clapack.cpp
+++ time_clapack.cpp
@@ -3,7 +3,7 @@
 #include <chrono>
 #include <stdexcept>
 #include <sstream>
-#include <vector>
+#include <valarray>
 
 extern "C" {
   void dgemm_(char* TRANSA, char* TRANSB, int* M, int* N, int* K, double* ALPHA,
@@ -29,9 +29,9 @@ int main(int argc, char* argv[]) {
   std::cout << "Matrix size N: " << N << ", N_matmuls: " << N_matmuls <<
     ", N_solves: " << N_solves << std::endl;
   
-  std::vector<double> m1(N * N, 0.0);
-  std::vector<double> m2(N * N, 0.0);
-  std::vector<double> m3(N * N, 0.0);
+  std::valarray<double> m1(N * N);
+  std::valarray<double> m2(N * N);
+  std::valarray<double> m3(N * N);
   std::time_t start_time, end_time;
   char TRANSA = 'N', TRANSB = 'N';
   double ALPHA = 1.0, BETA = 0.0;
@@ -39,10 +39,10 @@ int main(int argc, char* argv[]) {
   start_time = std::clock();
   auto start = std::chrono::steady_clock::now();
   for(int j = 1; j <= N_matmuls; j++) {
-    m1.assign(N * N, 1.0);
-    m2.assign(N * N, 2.0);
+    m1 = 1.0;
+    m2 = 2.0;
     // m3 = m1 * m2;
-    dgemm_(&TRANSA, &TRANSB, &N, &N, &N, &ALPHA, m1.data(), &N, m2.data(), &N, &BETA, m3.data(), &N);
+    dgemm_(&TRANSA, &TRANSB, &N, &N, &N, &ALPHA, &m1[0], &N, &m2[0], &N, &BETA, &m3[0], &N);
   }
   end_time = std::clock();
   auto end = std::chrono::steady_clock::now();
@@ -54,15 +54,15 @@ int main(int argc, char* argv[]) {
 	    << double(end_time - start_time) / CLOCKS_PER_SEC << std::endl;
   if (N <= 10) {
     std::cout << "Result of matmul:";
-    for (int j = 0; j < N * N; j++) std::cout << " " << m3.at(j);
+    for (int j = 0; j < N * N; j++) std::cout << " " << m3[j];
     std::cout << std::endl;
   }
   
   /////////////////////////////////////////////
 
-  std::vector<double> v1(N, 0.0);
+  std::valarray<double> v1(N);
   int index, j1, j2;
-  std::vector<int> IPIV(N, 0);
+  std::valarray<int> IPIV(N);
   int one = 1, INFO;
 
   /////////////////////////////////////////////
@@ -70,7 +70,7 @@ int main(int argc, char* argv[]) {
   start_time = std::clock();
   start = std::chrono::steady_clock::now();
   for(int j = 1; j <= N_solves; j++) {
-    v1.assign(N, 1.0);
+    v1 = 1.0;
     index = -1;
     for(j1 = 0; j1 < N; j1++) {
       for(j2 = 0; j2 < N; j2++) {
@@ -81,7 +81,7 @@ int main(int argc, char* argv[]) {
       m1[j1 + N * j1] = j1 + 1;
     }
     // std::cout << "Matrix:" << std::endl << m1 << std::endl;
-    dgesv_(&N, &one, m1.data(), &N, IPIV.data(), v1.data(), &N, &INFO);
+    dgesv_(&N, &one, &m1[0], &N, &IPIV[0], &v1[0], &N, &INFO);
   }
   end_time = std::clock();
   end = std::chrono::steady_clock::now();
@@ -92,7 +92,7 @@ int main(int argc, char* argv[]) {
 	    << double(end_time - start_time) / CLOCKS_PER_SEC << std::endl;
   if (N <= 10) {
     std::cout << "Solution of linear system:";
-    for (int j = 0; j < N; j++) std::cout << " " << v1.at(j);
+    for (int j = 0; j < N; j++) std::cout << " " << v1[j];
     std::cout << std::endl;
   }
   
